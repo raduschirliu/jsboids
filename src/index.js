4eba10f908f33f5ebe3a7e6fef254c11159aedd9
Radu Schirliu <radu.schirliu@gmail.com>
2019-10-08 22:02:56 -0600

    Further testing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import p5 from "p5";
 
 new p5(p => {
+  let x = 0;
+
   p.setup = () => {
     p.createCanvas(400, 400);
+    p.angleMode(p.DEGREES);
   }
 
   p.draw = () => {
     p.background(255, 255, 255);
+    
+    p.push();
+    p.translate(100, 100);
+    p.rotate(x);
+
     p.fill(20, 200, 20);
-    p.rect(100, 100, 500, 50);
+    p.triangle(0, -50, 50, 50, -50, 50);
+    
+    p.fill(200, 20, 20);
+    p.ellipse(0, 0, 5, 5);
+    p.pop();
+
+    x++;
   }
 });
