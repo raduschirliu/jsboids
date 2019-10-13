6e61b687d829eb6166bdb8a081568ecf77e0723d
Radu <radu.schirliu@gmail.com>
2019-10-13 11:06:18 -0600

    Added better graphics, start on movement

--- src/boid.js
+++ src/boid.js
@@ -1,11 +1,37 @@
 export default class Boid {
   constructor() {
-    this.pos = p.createVector(p.random(p.width), p.random(p.height));
-    this.color = p.color(p.random(255), p.random(255), p.random(255));
+    this.pos = p.createVector(p.random(20, p.width - 20), p.random(20, p.height - 20));
+    this.color = p.color(20, 200, 200);
+    this.angle = 0;
+    this.debug = false;
+    this.speed = 3;
+  }
+
+  update() {
+    this.pos.add(p.createVector(this.speed, this.speed));
+
+    if (this.pos.x > p.width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = p.width;
+    if (this.pos.y > p.height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = p.height;
   }
 
   draw() {
+    p.push();
+    p.translate(this.pos.x, this.pos.y);
+    p.rotate(this.angle);
+    
+    if (this.debug) {
+      p.noStroke();
+      p.fill(200, 20, 20, 100);
+      p.ellipse(0, 0, 40, 40);
+      p.stroke(0);
+    }
+
     p.fill(this.color);
-    p.ellipse(this.pos.x, this.pos.y, 20, 20);
+    p.triangle(0, -15, 10, 10, -10, 10);
+
+    p.ellipse(0, 0, 1, 1);
+    p.pop();
   }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -7,16 +7,21 @@ export default class Sketch {
   }
 
   setup() {
-    p.createCanvas(800, 600);
+    p.createCanvas(1000, 650);
     p.angleMode(p.DEGREES);
 
     for (let i = 0; i < this.amount; i++) {
       this.boids.push(new Boid());
     }
+
+    this.boids[0].debug = true;
   }
 
   draw() {
+    p.background(240, 240, 240);
+    
     for (let i = 0; i < this.boids.length; i++) {
+      this.boids[i].update();
       this.boids[i].draw();
     }
   }
